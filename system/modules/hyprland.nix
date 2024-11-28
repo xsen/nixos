ac714d3f3be8f4eedf6fc2579a3b952d7c07db75
Evgeny Leshchenko <evgeny.leshchenko@gmail.com>
2024-11-28 23:20:13 +0500

    tmp commit

---
+++ system/modules/hyprland.nix
@@ -0,0 +1,96 @@
+{ config, pkgs, ... }:
+
+{
+	security.polkit.enable = true;
+	security.pam.services.swaylock = {};
+	security.pam.services.swaylock.fprintAuth = false;
+        
+	services.xserver.displayManager = {
+		sddm.enable = true;
+		sddm.theme = "${import ./sddm-theme.nix { inherit pkgs; }}";
+	};
+
+	xdg.portal = {
+		enable = true;
+		wlr.enable = false;
+		xdgOpenUsePortal = false;
+		extraPortals = [ pkgs.xdg-desktop-portal-hyprland
+			pkgs.xdg-desktop-portal-gtk
+		];
+	};
+
+	programs = {
+		hyprland = {
+			enable = true;
+
+			xwayland = {
+			    enable = true;
+			};
+
+			portalPackage = pkgs.xdg-desktop-portal-hyprland;
+		};
+	};
+
+	systemd = {
+		user.services.polkit-gnome-authentication-agent-1 = {
+			description = "polkit-gnome-authentication-agent-1";
+			wantedBy = [ "graphical-session.target" ];
+			wants = [ "graphical-session.target" ];
+			after = [ "graphical-session.target" ];
+			serviceConfig = {
+				Type = "simple";
+				ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+				Restart = "on-failure";
+				RestartSec = 1;
+				TimeoutStopSec = 10;
+			};
+		};
+		extraConfig = '' DefaultTimeoutStopSec=10s '';
+	};
+
+	environment.systemPackages = with pkgs; [
+		hyprpaper
+		kitty
+		libnotify
+		mako
+		qt5.qtwayland
+		qt6.qtwayland
+		swayidle
+		swaylock-effects
+		wlogout
+		wl-clipboard
+		wofi
+		waybar
+
+		btop
+		eza
+		fzf
+		gnumake
+		lm_sensors
+		libsForQt5.qt5.qtquickcontrols2
+		libsForQt5.qt5.qtgraphicaleffects
+		libsForQt5.qt5.qtsvg
+		neofetch
+		neovim
+		ripgrep
+		tldr
+		unzip
+		openssl
+		openssl.dev
+		pkg-config
+		xfce.thunar
+		xdg-desktop-portal-gtk
+		xdg-desktop-portal-wlr
+		zip
+		zoxide
+
+		polkit
+		polkit_gnome
+
+		gnome3.adwaita-icon-theme # default gnome cursors
+		glib 
+		gsettings-desktop-schemas
+		nwg-look
+	];
+
+}
--- system/modules/packages.nix
+++ system/modules/packages.nix
@@ -1,15 +1,11 @@
 { config, inputs, pkgs, ...}: {
 
-  programs.hyprland.enable = true;
-
   environment.systemPackages = with pkgs; [
     home-manager
-    git
     wget
-    kitty
+    git
     vim 
-    wofi
-    waybar
+
     inputs.yandex-browser.packages.x86_64-linux.yandex-browser-stable
     telegram-desktop
     google-chrome
