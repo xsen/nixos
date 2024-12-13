23940ccb7a6890667777325d32e8baaa7b15657d
xsen <evgeny.leshchenko@gmail.com>
2024-12-13 13:04:54 +0500

    add ideavimrc file

--- home/modules/dotfiles.nix
+++ home/modules/dotfiles.nix
@@ -3,21 +3,11 @@ let
 in
 {
   home.file = {
-    #".config/nvim".source = "${configDir}/nvim";
-    #".config/wallpapers".source = "${configDir}/wallpapers";
-    #".config/kitty".source = "${configDir}/kitty";
-    #".config/neofetch".source = "${configDir}/neofetch";
+    ".ideavimrc".source = "${configDir}/ideavimrc";
     ".config/hypr".source = "${configDir}/hypr";
     ".config/.oh-my-zsh" = {
       recursive = true;
       source = "${configDir}/catppuccin-zsh/";
     };
-    #".config/swayidle".source = "${configDir}/swayidle";
-    #".config/swaylock".source = "${configDir}/swaylock";
-    #".config/wlogout".source = "${configDir}/wlogout";
-    #".config/waybar".source = "${configDir}/waybar";
-    #".config/btop".source = "${configDir}/btop";
-    #".config/wofi".source = "${configDir}/wofi";
-    #".config/mako".source = "${configDir}/mako";
   };
 }
